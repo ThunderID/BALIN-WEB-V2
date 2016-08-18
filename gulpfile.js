61917c7cf6c0f05dc3b778f81a0b0f98e8be421c
budi7 <budi-purnomo@outlook.com>
2016-08-18 11:55:24 +0700

    init project

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Less
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir.config.sourcemaps = false;
+elixir(function(mix) {
+		mix.sass('web_v2/balin.scss', 'public/css/balin.css')
+		.scripts(['jquery.js', 
+				'jquery.validate.min.js',
+				'bootstrap.min.js',
+				'icheck.min.js',
+				'inputmask.js',
+				'quantity_control.js',
+				'owl_carousel.js',
+				'checkout_plugin.js',
+				'easyzoom.js',
+				'all.js',
+				], 'public/js/balin.js')
+		.version(['public/css/balin.css',
+				'public/js/balin.js'])
+		.copy('resources/assets/fonts', 'public/build/fonts/')
+		.copy('resources/assets/plugins/', 'public/plugins/')
+		.copy('resources/assets/images/', 'public/images/');
+});
