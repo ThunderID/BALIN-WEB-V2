74c027b94baefaf3d7112243fcb5d0dd645ecea1
budi7 <budi-purnomo@outlook.com>
2016-08-25 10:55:06 +0700

    fixed missing breadcrumb on product show

--- resources/views/web_v2/desktop/products/show.blade.php
+++ resources/views/web_v2/desktop/products/show.blade.php
@@ -1,11 +1,10 @@
 @extends('web_v2.page_templates.layout')
 
 @section('content')
-	<?php 
-	// dd($data); 
+	@include('web_v2.components.category-desktop')
+	@include('web_v2.components.breadcrumb')
 
-	?>
-	<section class="container">
+	<section class="container mt-0 mb-lg">
 		<div class="row">
 			<!-- SECTION IMAGE SLIDER PRODUCT -->
 			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 text-center">
