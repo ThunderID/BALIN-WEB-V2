4be3987812481b169c54386b8d813033174071f9
budi7 <budi-purnomo@outlook.com>
2016-09-01 12:11:49 +0700

    fixed page akun login & reset password

--- resources/views/web_v2/components/forgot/form.blade.php
+++ resources/views/web_v2/components/forgot/form.blade.php
@@ -1,10 +1,14 @@
 {!! Form::open(['url' => route('balin.forgot.password'), 'class' => 'form']) !!}
-	<div class="form-group">
-		<label for="" style="font-weight:400">Email</label>
+    <div class="form-group input-group">
+		<span class="input-group-addon" id="basic-addon1">
+			<div class="text-center" style="width:30px;">
+				<i class="fa fa-envelope fa-lg aria-hidden="true"></i>
+			</div>
+		</span>
 		{!! Form::email('email', null, ['class' => 'form-control hollow', 'placeholder' => 'Masukkan Email', 'required']) !!}
-	</div>
+    </div>	
 	<div class="form-group text-right">
 		<a href="#" class="hover-grey btn-cancel">Cancel</a>&nbsp;&nbsp;&nbsp;
-		<button type="submit" class="btn btn-black-hover-white-border-black">Simpan</button>
+		<button type="submit" class="btn btn-orange pl-xl pr-xl">Reset</button>
 	</div>
 {!! Form::close() !!}
