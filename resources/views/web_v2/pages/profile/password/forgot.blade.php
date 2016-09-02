fdd155e76676d3f0d24f76d7ed1e050408fc5ddb
chelsymooy <chelsymooy1108@gmail.com>
2016-09-02 16:29:21 +0700

    Somehow reset password fixed

--- resources/views/web_v2/pages/profile/password/forgot.blade.php
+++ resources/views/web_v2/pages/profile/password/forgot.blade.php
@@ -2,12 +2,15 @@
 
 @section('content')
 	<div class="clearfix">&nbsp;</div>
-	<div class="row bg-grey-light ml-0 mr-0 pb-md">
-	<!-- SECTION ACTIVATION LINK -->
-		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-			<h3 class="">Permintaan Reset Password Diterima</h3> 
-			<p class="mb-md mt-md">Silahkan periksa email Anda, Balin telah mengirimkan link reset password.</p> 
+	<div class="clearfix hidden-xs hidden-sm">&nbsp;</div>
+	<section class="container">
+		<div class="row ml-0 mr-0 pb-md">
+		<!-- SECTION ACTIVATION LINK -->
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<h3 class="">Permintaan Reset Password Diterima</h3> 
+				<p class="mb-md mt-md">Silahkan periksa email Anda, Balin telah mengirimkan link reset password.</p> 
+			</div>
 		</div>
-	</div>
-	<!-- END SECTION REFERRAL CODE -->
+		<!-- END SECTION REFERRAL CODE -->
+	</section>
 @stop
--- resources/views/web_v2/pages/profile/password/reset.blade.php
+++ resources/views/web_v2/pages/profile/password/reset.blade.php
@@ -1,36 +1,31 @@
 @extends('web_v2.page_templates.layout')
 
 @section('content')
-	<div class="container-fluid">
+	<div class="container-fluid background">
 		<div class="row">
-			<div class="hidden-xs hidden-sm col-md-7 col-lg-7">&nbsp;</div>
-			<div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
-				<div class="row panel panel-default p-xs mt-md">
-					<div class="col-md-12">
-						<h3>Reset Password</h3>
-						{!! Form::open(['url' => route('balin.change.password'), 'method' => 'POST']) !!}
-							<div class="form-group">
-								<label class="mt-md">Password</label>
-								{!! Form::password('password', ['class' => 'form-control hollow', 'placeholder' => 'Masukkan password', 'tabindex' => '1']) !!}
-							</div>
-							<div class="form-group">
-								<label>Konfirmasi Password</label>
-								{!! Form::password('password_confirmation', ['class' => 'form-control hollow', 'placeholder' => 'Masukkan konfirmasi password', 'tabindex' => '2']) !!}
-							</div>
-							<div class="form-group text-right">
-								<button type="submit" class="btn btn-black-hover-white-border-black" tabindex="3">Simpan</button>
-							</div>
-						{!! Form::close() !!}
-					</div>
-				</div>
+			{{-- empty area --}}
+			<div class="hidden-xs hidden-sm col-md-7 col-lg-7 text-center preface">
+			</div>
+
+			{{-- reset password --}}
+			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+				<div class="row panel-akun p-xs mt-md">
+					<div class="col-md-12 text-center">
+						<h2 class="text-center mb-lg">Ubah Password</h2>
+						@include('web_v2.components.alert-box')
+						@include('web_v2.components.reset.form')
+					</div>	
+					<div class="clearfix">&nbsp;</div>
+				</div>                        
 			</div>
 		</div>
-		<div class="clearfix">&nbsp;</div>
-		<div class="clearfix">&nbsp;</div>
 	</div>
 @stop
 
-
 @section('wrapper_class')
 	bg-login-page
+@stop
+
+@section('script_plugin')
+
 @stop
