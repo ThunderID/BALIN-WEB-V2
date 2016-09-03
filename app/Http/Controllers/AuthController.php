742d03d0dcdfcf2d979412550644baedf18adcfe
chelsymooy <chelsymooy1108@gmail.com>
2016-09-03 10:45:16 +0700

    bug on mobile sign in

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -35,7 +35,7 @@ class AuthController extends BaseController
 	{
 		if (preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]))
 		{
-			$dob						= Carbon::createFromFormat('Y-m-d', Input::get('dob'))->format('Y-m-d H:i:s');
+			$dob						= Carbon::createFromFormat('d-m-Y', Input::get('dob'))->format('Y-m-d H:i:s');
 		}
 		else
 		{
