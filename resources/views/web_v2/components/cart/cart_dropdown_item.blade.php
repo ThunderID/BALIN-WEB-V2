ac0a3dc2b4ec93b9d895e545447c210fed7267c9
budi7 <budi-purnomo@outlook.com>
2016-09-06 16:00:41 +0700

    enhance shopping cart desktop

--- resources/views/web_v2/components/cart/cart_dropdown_item.blade.php
+++ resources/views/web_v2/components/cart/cart_dropdown_item.blade.php
@@ -67,23 +67,24 @@
 					</div>
 				</div>
 				<!-- END SECTION PRICE PER PRODUCT -->
+				<!-- SECTION DISCOUNT PER PRODUCT -->
+				<div class="row text-regular">
+					<div class="col-xs-12">
+						<div class="row">
+							<div class="col-xs-2">
+								<span class="info">Disc</span>
+							</div>
+							<div class="col-xs-1">
+								<span class="info">:</span>
+							</div>
+							<div class="col-xs-8 text-right">
+								<span class="info">@money_indo($label_discount * $qty)</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- END SECTION DISCOUT -->
 				<?php
-				// <!-- SECTION DISCOUNT PER PRODUCT -->
-				// <div class="row text-regular">
-				// 	<div class="col-xs-12">
-				// 		<div class="row">
-				// 			<div class="col-xs-2">
-				// 				<span class="info">Disc</span>
-				// 			</div>
-				// 			<div class="col-xs-1">
-				// 				<span class="info">:</span>
-				// 			</div>
-				// 			<div class="col-xs-8 text-right">
-				// 				<span class="info">@money_indo($label_discount)</span>
-				// 			</div>
-				// 		</div>
-				// 	</d
-				// <!-- END SECTION DISCOUT -->
 				// <!-- SECTION TOTAL PRICE PRODUCT -->
 				// <div class="row text-regular">
 				// 	<div class="col-xs-12"> 
