a77941e52f86345dd37775ee844000b6f3798821
chelsymooy <chelsymooy1108@gmail.com>
2016-09-07 14:33:38 +0700

    Working #10

--- resources/views/web_v2/components/nav.blade.php
+++ resources/views/web_v2/components/nav.blade.php
@@ -102,7 +102,17 @@
 		<div class="text-center center-nav desktop-only" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav navbar-right">
 				<li class="text-light right">
-					<a href="{{ route('balin.product.index', ['categories[]' => 'wanita']) }}" class=" @if(isset(Input::get('categories')[0]) && Input::get('categories')[0] == 'wanita') text-orange @endif">
+					<?php $extend_search = [];?>
+
+					@if(Input::has('tags'))
+						<?php $extend_search = array_merge($extend_search, Input::only('tags'));?>
+					@endif
+
+					@if(Input::has('sort'))
+						<?php $extend_search = array_merge($extend_search, Input::only('sort'));?>
+					@endif
+
+					<a href="{{ route('balin.product.index', array_merge(['categories[]' => 'wanita'], $extend_search) ) }}" class=" @if(isset(Input::get('categories')[0]) && Input::get('categories')[0] == 'wanita') text-orange @endif">
 						Wanita
 						<span>
 							{!! HTML::image('images/woman_white.png', null, ['class' => 'img-responsive']) !!}
@@ -114,7 +124,7 @@
 					</div>
 				</li>>
 				<li class="text-light left">
-					<a href="{{ route('balin.product.index', ['categories[]' => 'pria']) }}" class=" @if(isset(Input::get('categories')[0]) && Input::get('categories')[0] == 'pria') text-orange @endif">
+					<a href="{{ route('balin.product.index', array_merge(['categories[]' => 'pria'], $extend_search) ) }}" class=" @if(isset(Input::get('categories')[0]) && Input::get('categories')[0] == 'pria') text-orange @endif">
 						<span>
 							{!! HTML::image('images/man_white.png', null, ['class' => 'img-responsive']) !!}
 						</span>
