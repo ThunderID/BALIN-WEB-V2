510aed899ac64ff84b27071a2c3139933259ffe2
budi7 <budi-purnomo@outlook.com>
2016-09-13 14:25:30 +0700

    enhancement - repair mobile navbar link - submenu top location

--- resources/views/web_v2/components/nav.blade.php
+++ resources/views/web_v2/components/nav.blade.php
@@ -43,10 +43,10 @@
 			</ul>
 			<ul class="nav navbar-nav navbar-right" >
 				<li class="hidden-md hidden-lg">
-					<a href="" class="hover-orange">Koleksi Wanita</a>
+					<a href="{{ route('balin.product.index', array_merge(['categories[]' => 'wanita']) ) }}" class="hover-orange">Koleksi Wanita</a>
 				</li>
 				<li class="hidden-md hidden-lg">
-					<a href="" class="hover-orange">Koleksi Pria</a>
+					<a href="{{ route('balin.product.index', array_merge(['categories[]' => 'pria']) ) }}" class="hover-orange">Koleksi Pria</a>
 				</li>
 				<li>
 					@if (Session::has('whoami'))
