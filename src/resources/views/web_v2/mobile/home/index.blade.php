ef901b64163400d1e5c283b6ba80d3bd7d913b1d
Agil Mahendra <agil.mahendra@gmail.com>
2017-01-05 16:46:09 +0700

    repair file index home replace with product index

--- src/resources/views/web_v2/mobile/home/index.blade.php
+++ src/resources/views/web_v2/mobile/home/index.blade.php
@@ -49,133 +49,49 @@
 				</div>
 			</div>
 		</div>
+		 <div class="clearfix">&nbsp;</div>
 	</section>
-	<section class="container pt-xl mb-xs relative" style="margin-top: 110px;">
-		<div class="ajax-loading pt-xl" style="display:none; margin-top: 50px;">
-			<img src="/images/loading-balin.gif" />
-			<h4>
-				All in good time ...</br>
-				<small>a moment till we're ready</small>
-			</h4>
-		</div>
-		<div class="content-data" data-title="{{ isset($page_subtitle) ? $page_subtitle . ' - ' . $page_title : 'BALIN.ID' }}">
-			@if(count($data['offer']))
-				<div class="row">
-					<div class="col-md-12 text-center">
-						<h3 class="">COMING SOON</h3>
-						<h5 class="">Please stay tuned to be the first to know when our product is ready</h5>
-					</div>
-				</div>
-				<div class="clearfix">&nbsp;</div>
-				<div class="row">
-					<div class="col-md-12">
-						<h4 class="mt-md mb-sm pl-sm pr-sm">Anda Mungkin Suka</h4>
-					</div>
-				</div>
-				<div class="row row-card mt-md mb-sm pl-sm pr-sm">
-					{{-- DATA GRID CARD PRODUCT --}}
-					@include('web_v2.components.card', [
-						'card' 	=> $data['offer'],
-				  		'col'	=> 'col-xs-6 col-sm-6',
-				  		'last' => true
-					])
-				</div>
-				<div class="col-md-12 text-center">
-					<a class="btn btn-orange" href="{{route('balin.product.index', $data['linked_search'])}}">Lihat Semua</a>
-				</div>
-			@else
-				<div class="row pb-xs">
-					<div class="col-xs-12 col-sm-12 text-right">
-						<span class="pt-xs">Showing {{ $paging_from }} - {{ $paging_to }} of {{ $paging->total() }} results</span>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-xs-12 col-sm-12">
-						<div class="row">
-							@include('web_v2.components.card', [
-								'card' 	=> $data['product'],
-						  		'col'	=> 'col-xs-6 col-sm-6 '
-							])
-						</div>
-					</div>
-				</div>
-			@endif
+	@endif
 
-			<div class="row">
-				<div class="col-xs-12 col-sm-12">
-					@include('web_v2.components.ajax_page')
+	@if(isset($data['instagram']))
+	<section class="container mt-xl">
+		<div class="container">
+			<div class="row pb-xs">
+				<div class="col-md-12 text-center">
+					<p class="m-0">Make sure you follow us on</p>
+					<h3 class="text-uppercase mtm-5 mbm-5">
+						<a href="{{$balin['info']['instagram_url']['value']}}" class="hover-orange" target="_blank">
+							<i class="fa fa-instagram" aria-hidden="true"></i>
+							balin.id
+						</a>
+					</h3>
 				</div>
 			</div>
-			<div class="row">
-				@include('web_v2.plugins.ajaxPaging')
+			<div class="row pt-md pb-md mb-sm instagram-mobile">
+				<div class="row">
+					<div class="carousel">
+			 			@foreach($data['instagram'] as $key => $data)
+							<?php $link = json_decode($data['value'], true); ?>
+							<a class="link" href="{{$link['action']}}" target="_blank">
+								<div class="item">
+									<div class="col-xs-12 pl-0 pr-0">
+										<div class="tile text-center">
+											{!! Html::image($data['image_sm'], null, ['class' => 'img-responsive ig_mobile']) !!}
+											<div class="jumbotron">
+											</div>
+										</div>
+									</div>
+								</div>
+							</a>
+						@endforeach
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
+	@endif
 @stop
 
 @section('js_plugin')
-@stop
-
-@section('js')  
-	$(document).ready(function(){
-		{{-- FUNCTION COLLAPSE CATEGORY, FILTER, SORT --}}
-		$('.panel').on('hide.bs.collapse', function (e) {
-			$(e.currentTarget).find('span').removeClass('active');
-		});
-		$('.panel').on('show.bs.collapse', function (e) {
-			$(e.currentTarget).find('span').addClass('active');
-		});
-
-		{{-- FUNCTION CHECKBOX FILTER COLOR CLICK --}}
-		$('span.color-item').click(function(){
-			item = $(this).parent();
-			color = $(this).attr('data-color');
-			checkboxcolor = $(this).parent().find('.checkbox-color');
-
-			if (item.hasClass('active')) {
-				item.removeClass('active');
-				checkboxcolor.prop('checked', false);
-			} else {
-				item.addClass('active');
-				checkboxcolor.prop('checked', true);
-			}
-			ajaxFilter(checkboxcolor);
-		});
-
-		{{-- filter clear all mobile --}}
-		$('.clearall-filter-mobile').click(function(){
-			// to ajax paging
-			ajaxPaging($(this));
-
-			// remove filter & sort active
-			$('.filter-info').html('');
-			$('.sort-info').html('');
-			$('.checkbox-filter').prop('checked', false);
-			$('.checkbox-color').prop('checked', false);
-			$('span.color-item').parent().removeClass('active');
-		});
-	});
-
-	{{-- event filter active click --}}
-	$('h4.panel-title').on('click', 'label.panel-action', function(e) {
-		stop_double_event(e, $(this));
-	});
-
-	{{-- btn share --}}
-	$('.btn-copy-share').attr('data-clipboard-text', window.location.href);
-	fb_link = $('.btn-facebook-share').attr('href');
-	$('.btn-facebook-share').attr('href', fb_link + '&href=' +window.location.href);
-
-	{{-- event click more in active filter --}}
-	function click_more(param, e) {
-		e.stopPropagation();
-		if ($('.filter-more').hasClass('hide')) {
-			$('.filter-more').removeClass('hide');
-			param.html('Less..');
-		} else {
-			$('.filter-more').addClass('hide');
-			param.html('More..');
-		}
-	}
-	$('.more').on('click', function(e){click_more($(this), e)});
+	@include('web_v2.plugins.owlCarousel')
 @stop
